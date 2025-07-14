1e5f038b543f5aad02bf438ec3fc69b157aee060
Ziga Mahne <ziga.mahne@arm.com>
2025-07-14 14:20:35 +0200

    Update VIO Driver
    - add support for LED LD1

--- CMSIS/Driver/vio_STM32F746G-DISCO.c
+++ CMSIS/Driver/vio_STM32F746G-DISCO.c
@@ -1,11 +1,11 @@
 /******************************************************************************
  * @file     vio_STM32F746G-DISCO.c
  * @brief    Virtual I/O implementation for board STM32F746G-DISCO
- * @version  V2.0.2
- * @date     26. September 2024
+ * @version  V2.1.0
+ * @date     14. July 2025
  ******************************************************************************/
 /*
- * Copyright (c) 2020-2024 Arm Limited (or its affiliates).
+ * Copyright (c) 2020-2025 Arm Limited (or its affiliates).
  * All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -30,6 +30,7 @@ The table below lists the physical I/O mapping of this CMSIS-Driver VIO implemen
 | Virtual I/O   | Variable       | Board component      | Pin
 |:--------------|:---------------|:---------------------|:------
 | vioBUTTON0    | vioSignalIn.0  | USER button (B1)     | PI11
+| vioLED0       | vioSignalOut.0 | LED Green   (LD1)    | PI1
 */
 
 #include "cmsis_vio.h"
@@ -66,7 +67,12 @@ typedef struct {
 
 #if !defined CMSIS_VOUT
 // VOUT Configuration
-static const pinCfg_t outputCfg[] = {};
+static const pinCfg_t outputCfg[] = {
+#if defined USE_LED_LD1
+//  signal,     pin,                   pull resistor,      active state
+  { vioLED0,    GPIO_PIN_ID_PORTI(1),  ARM_GPIO_PULL_NONE, VIO_ACTIVE_HIGH }
+#endif
+};
 #endif
 
 #if !defined CMSIS_VIN
